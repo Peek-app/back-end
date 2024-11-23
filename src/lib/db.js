26f20eeaf3ef22a858478782a43d00e1f39ba00a
Arturo Juarez <ajrobles98@gmail.com>
2024-11-23 17:03:17 -0600

    feat : adding server configuration and DB connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
+const DB_CONNECTION = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
-const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+//const DB_CONNECTION = `mongodb+srv://peekapp04:PeekApp02Kodemia@peek-cluster.jp3x3.mongodb.net/`;
 
 function connect() {
-  return mongoose.connect(url);
+  return mongoose.connect(DB_CONNECTION);
 }
-
 module.exports = { connect };
---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const app = express();
+/*const usersRoutes = require("../routes/users.router");
+const postRoutes = require("../routes/posts.router");
+const authRoutes = require("../routes/auth.router");
+*/
+app.use(
+  cors({
+    //origin: "https://dev-to-arturo-juarezs-projects.vercel.app",
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(helmet());
+app.use(express.json());
+/*
+app.use("/users", usersRoutes);
+app.use("/posts", postRoutes);
+app.use("/auth", authRoutes);
+*/
+app.get("/", (request, response) => {
+  response.json({
+    success: true,
+    message: "Back-End Peek App",
+  });
+});
+
+module.exports = app;
