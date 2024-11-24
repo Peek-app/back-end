99a7f87dedbb2f4cbddde072b3fd0f22468d59ed
Arturo Juarez <ajrobles98@gmail.com>
2024-11-23 18:32:33 -0600

    feat : adding aditional configuration for server

--- src/lib/db.js
+++ src/lib/db.js
@@ -7,8 +7,6 @@ const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 const DB_CONNECTION = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
-//const DB_CONNECTION = `mongodb+srv://peekapp04:PeekApp02Kodemia@peek-cluster.jp3x3.mongodb.net/`;
-
 function connect() {
   return mongoose.connect(DB_CONNECTION);
 }
