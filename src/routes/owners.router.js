ab7c472ea764526046a51384e25c5aa029f0ebd8
Arturo Juarez <ajrobles98@gmail.com>
2024-11-27 19:57:22 -0600

    feat : adding middleware and verifications to create owner

--- src/routes/owners.router.js
+++ src/routes/owners.router.js
@@ -2,10 +2,20 @@ const express = require("express");
 const ownerUseCases = require("../usecases/owners.usecases");
 const createError = require("http-errors");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const vetUseCases = require("../usecases/vets.usecases");
 
-router.post("/", async (request, response) => {
+router.post("/", auth, async (request, response) => {
   try {
     const ownerData = request.body;
+    ownerData.user = request.user?.id;
+
+    const existingVet = await vetUseCases.getById(ownerData.user);
+    const existingOwner = await ownerUseCases.getById(ownerData.user);
+    if (existingVet || existingOwner) {
+      throw createError(404, "User alredy has a role");
+    }
+
     const newOwner = await ownerUseCases.create(ownerData);
     response.json({
       success: true,
